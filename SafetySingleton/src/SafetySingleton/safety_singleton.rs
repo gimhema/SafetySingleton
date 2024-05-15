21a726294595da7b6a1f220bfa13caa8c097eeea
gimhema <49871739+gimhema@users.noreply.github.com>
2024-05-15 01:46:45 +0000

    use Arc

--- SafetySingleton/src/SafetySingleton/safety_singleton.rs
+++ SafetySingleton/src/SafetySingleton/safety_singleton.rs
@@ -1,20 +1,15 @@
-use std::sync::{Mutex, Arc};
-use std::thread;
-
+use std::sync::{RwLock, Arc};
 
 pub struct SafetySingleton {
     pub data: i32,
 }
 
 lazy_static! {
-    static ref INSTANCE: Mutex<SafetySingleton> = Mutex::new(SafetySingleton { data: 0 });
+    static ref INSTANCE: Arc<RwLock<SafetySingleton>> = Arc::new(RwLock::new(SafetySingleton { data: 0 }));
 }
 
 impl SafetySingleton {
-    pub fn get_instance() -> &'static Mutex<SafetySingleton> {
+    pub fn get_instance() -> &'static Arc<RwLock<SafetySingleton>> {
         &INSTANCE
     }
 }
-
-
-
